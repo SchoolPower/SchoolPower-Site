80dcbea36fcf961bbfc9b6ae6a4504bc43d93ae2
CarbonylGroup <carbonylgp@gmail.com>
2021-12-13 23:46:08 -0500

    wording

--- src/i18n/resources/ja.ts
+++ src/i18n/resources/ja.ts
@@ -81,7 +81,7 @@ const ja: ResourceLanguage = {
                 instruction: {
                     desktop: "カメラを使用して、Wechat でこのQRコードをスキャンする",
                     mobile: "このQRコードを保存し、Wechat で「アルバムから選択」を選択する" +
-                        "（iOS Safariユーザーは、保存するために画像を手動で長押しする必要がある場合があります）"
+                        "（iOS Safariを使う方は、保存するために画像を手動で長押しする必要がある場合があります）"
                 }
             },
             alipay: {
@@ -89,7 +89,7 @@ const ja: ResourceLanguage = {
                 instruction: {
                     desktop: "カメラを使用して、Alipay でこのQRコードをスキャンする",
                     mobile: "このQRコードを保存し、Alipay で「アルバムから選択」を選択する" +
-                        "（iOS Safariユーザーは、保存するために画像を手動で長押しする必要がある場合があります）"
+                        "（iOS Safariを使う方は、保存するために画像を手動で長押しする必要がある場合があります）"
                 }
             },
             paypal: {
